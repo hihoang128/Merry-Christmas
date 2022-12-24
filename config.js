d86ba825f27c0efb651c452d7e29a9b7f94a31af
hihoang128 <97720636+hihoang128@users.noreply.github.com>
2022-12-24 12:50:22 +0700

    Add files via upload
    Card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'ACE';
+const giftUrl = 'https://docs.google.com/forms/d/1Uss41DcokJrUdekmRJnbrG6pQzg0mk3T5FMlLI7iPGU/viewform?fbclid=IwAR058UiLJU61eNg4v1LGumOmLrvVDP9SFsOdZsP3ZRPYtcooh4pmhmdHrS0&edit_requested=true';
+const eventName = '♥️♥️♥️';
+const titleCard = 'Lời chúc đến bạn';
+const contentCard = 'Khi bạn nhận được tin nhắn này thì hãy cười đi nhé, vì ít nhất đâu đó quanh đây có một người mong bạn vui vẻ và luôn yêu đời. Mùa đông sẽ ấm áp hơn khi có một nụ cười. Rất may mắn khi được gặp bạn. Mong mọi điều an lành may mắn sẽ đến với bạn';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'https://scontent.fdad3-1.fna.fbcdn.net/v/t39.30808-6/321614331_568283558447738_2539043574064835923_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=Uhhfjnlh8ogAX--6C6J&_nc_ht=scontent.fdad3-1.fna&oh=00_AfAo6cxn8TPaN43MfJVgbetKtE1cSFwCBW8JOMOhbHHpyw&oe=63AB9426';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
